e308b55528062f894e319cfa41894a52cdd00aa8
BastianSucso <bastiansucsochoque@gmail.com>
2025-10-17 19:06:21 -0300

    actualización formato

--- src/app/pages/staff-list/staff-list.component.ts
+++ src/app/pages/staff-list/staff-list.component.ts
@@ -1,15 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { DataService, Staff } from '../../services/data.service';
 import { map, switchMap } from 'rxjs/operators';
-import { FormsModule } from '@angular/forms';
-import { Observable, combineLatest, of } from 'rxjs';
 
 @Component({
   standalone: true,
   selector: 'app-staff-list',
-  imports: [RouterLink, AsyncPipe, NgFor, NgIf, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './staff-list.component.html',
   styleUrls: ['./staff-list.component.css']
 })
@@ -18,10 +17,9 @@ export class StaffListComponent {
   private data = inject(DataService);
 
   q = '';
-  tipo$ = this.route.paramMap.pipe(map(p => p.get('tipo') as Staff['rol']));
-  base$: Observable<Staff[]> = this.tipo$.pipe(switchMap(t => this.data.getByRol(t)));
-  vm$ = combineLatest([of(null), this.base$]).pipe(map(([_, list]) =>
-    list.filter(s => (this.q ? (s.nombre + ' ' + (s.ubicacion||'')).toLowerCase().includes(this.q.toLowerCase()) : true))
-  ));
-  
+
+  list$ = this.route.paramMap.pipe(
+    map(p => p.get('tipo') as Staff['rol']),
+    switchMap(rol => this.data.getByRol(rol))
+  );
 }
